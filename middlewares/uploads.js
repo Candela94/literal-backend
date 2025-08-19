183326448678069bd9bb88608d02ebd4ae49495e
Candela94 <candelafsg@alumnos.cei.es>
2025-08-19 12:33:56 +0200

    uploads

--- middlewares/uploads.js
+++ middlewares/uploads.js
@@ -2,8 +2,15 @@
 
 import multer from "multer";
 import path from 'path'
+import fs from 'fs'
 
 
+
+const uploadDir = 'public/uploads/imagenes';
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 // 1. Configuramos almacenamiento de Multer
 
