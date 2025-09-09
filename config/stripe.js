e757c47f66706df6aff89568628f1eecd0c673ee
Candela94 <candelafsg@alumnos.cei.es>
2025-09-09 09:51:21 +0200

    stripe

--- config/stripe.js
+++ config/stripe.js
@@ -1,13 +1,13 @@
 
 
-import Stripe from "stripe";
-import dotenv from 'dotenv';
+// import Stripe from "stripe";
+// import dotenv from 'dotenv';
 
-dotenv.config();
-
-
+// dotenv.config();
 
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-export default stripe;
+
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+// export default stripe;
--- controllers/payment.controllers.js
+++ controllers/payment.controllers.js
@@ -1,33 +1,33 @@
 
 
 
-import stripe from "../config/stripe.js";
+// import stripe from "../config/stripe.js";
 
 
 
-export const createPaymentIntent = async (req,res) => {
+// export const createPaymentIntent = async (req,res) => {
 
 
-    try{
+//     try{
 
-        const {amount} = req.body;
+//         const {amount} = req.body;
 
-        const paymentIntent = await stripe.paymentIntents.create({
+//         const paymentIntent = await stripe.paymentIntents.create({
 
-            amount, //en cents: 5000 = 50e, lo recibo desde el front
-            currency:'eur',   
-            payment_method_types:['card'],
-        })
+//             amount, //en cents: 5000 = 50e, lo recibo desde el front
+//             currency:'eur',   
+//             payment_method_types:['card'],
+//         })
 
-        res.status(200).json({
+//         res.status(200).json({
 
-            clientSecret:paymentIntent.client_secret,  //client_secret es lo que usará React para finalizar el pago con tarjeta, el endpoint responde con este clientSecret
-        })
+//             clientSecret:paymentIntent.client_secret,  //client_secret es lo que usará React para finalizar el pago con tarjeta, el endpoint responde con este clientSecret
+//         })
 
 
 
-    }catch (error) {
-        console.error('❌ Error al crear el PaymentIntent:', error); // ✅
-        res.status(500).json({ error: error.message });
-      }
-}
+//     }catch (error) {
+//         console.error('❌ Error al crear el PaymentIntent:', error); // ✅
+//         res.status(500).json({ error: error.message });
+//       }
+// }
